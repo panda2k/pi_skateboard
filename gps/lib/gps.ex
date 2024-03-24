f0e975a7ae23515d93a29e675954f8d6d0a27fb0
panda2k <thelowkeypanda@gmail.com>
2024-03-23 23:10:11 -0700

    add message parsing for gps

--- gps/lib/gps.ex
+++ gps/lib/gps.ex
@@ -1,11 +1,16 @@
 defmodule Gps do
   use GenServer
+
   @moduledoc """
   Documentation for `Gps`.
   """
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, [], opts)
+  def start_link(opts \\ [message_limit: 20]) do
+    GenServer.start_link(
+      __MODULE__,
+      %{messages: :queue.new(), message_limit: opts[:message_limit], in_progress_message: ""},
+      opts
+    )
   end
 
   def init(_args) do
@@ -13,14 +18,60 @@ defmodule Gps do
     :ok = Nerves.UART.open(pid, "ttyAMA0", speed: 9600)
     {:ok, pid}
   end
+
+  def handle_info({:nerves_uart, _serial_port_id, data}, state) do
+    case build_message(data, state.in_progress_message) do
+      {existing_message} ->
+        {:noreply, Map.put(state, :in_progress_message, existing_message)}
+
+      {existing_message, new_message} ->
+        {
+          :noreply,
+          Map.put(state, :in_progress_message, new_message)
+          |> then(fn state -> 
+            case existing_message do 
+              << _head::binary-size(3), "GGA", _rest::binary >> -> 
+                Map.put(state, :messages, :queue.in(:binary.split(existing_message, ",", [:global]), state.messages))
+              _ -> state
+            end
+          end)
+          |> then(fn state ->
+            if :queue.len(state.messages) > state.message_limit do
+              Map.put(state, :messages, :queue.drop(state.messages))
+            else
+              state
+            end
+          end)
+        } 
+    end
+  end
    
-  def handle_info({:nerves_uart, serial_port_id, data}, state) do
-    IO.inspect data
-    {:noreply, state}
+  def build_message(new_fragment, existing_message) do
+    new_fragment = :binary.split(new_fragment, "\\r") 
+      |> Enum.flat_map(fn s -> :binary.split(s, "\\n") end)
+      |> Enum.flat_map(fn s -> :binary.split(s, "\n") end)
+      |> Enum.filter(fn s -> s != "" end)
+    case new_fragment do
+      # empty string so new message should be on next line
+      [] ->
+        {existing_message}
+    
+      # string has new GGA message 
+      [<<"$", new_message::binary>>] -> 
+        { existing_message, "$" <> new_message }
+      
+      # string has continuing message 
+      [msg] ->
+        if existing_message != "", do: {existing_message <> msg}, else: {existing_message}
+
+      # string has continuing message and start of new message (no new line)
+      [msg, <<"$", new_msg::binary>>] ->
+        {existing_message <> msg, "$" <> new_msg}
+    end
   end
 
   def handle_call(message, state) do
-    IO.inspect "Unhandled message: #{message}"
+    IO.inspect("Unhandled message: #{message}")
     {:noreply, state}
   end
 end
--- gps/test/gps_test.exs
+++ gps/test/gps_test.exs
@@ -2,7 +2,18 @@ defmodule GpsTest do
   use ExUnit.Case
   doctest Gps
 
-  test "greets the world" do
-    assert Gps.hello() == :world
+  test "handle_info" do 
+    default_state = %{
+      messages: :queue.new(),
+      message_limit: 50,
+      in_progress_message: ""
+    }
+
+    File.stream!("./test/gps_input.txt")
+      |> Enum.reduce(default_state, fn line, state -> 
+        {:noreply, state } = Gps.handle_info({ :nerves_uart, "doesn't matter", line }, state)
+        state
+      end)
+      |> IO.inspect()
   end
 end
